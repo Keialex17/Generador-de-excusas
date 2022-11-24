7f9c29563b68ea6d929b77207d266e02d58769cc
Keialex17 <hernandezkeiber34@gmail.com>
2022-11-24 14:53:36 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let generador = document.getElementById("excuse");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let excusasquien = ["the dog", "my granma", "his turtle", "my bird"];
+let excusasque = ["eat", "pissed", "crushed", "broked"];
+let excusascuando = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let randomExcusaQuien = Math.floor(Math.random() * excusasquien.length);
+
+let randomExcusaQue = Math.floor(Math.random() * excusasque.length);
+
+let randomExcusaCuandi = Math.floor(Math.random() * excusascuando.length);
+
+generador.innerHTML = ` <h3>${excusasquien[randomExcusaQuien]} ${excusasque[randomExcusaQue]} ${excusascuando[randomExcusaCuandi]} </h3>`;
+
+let refresh = document.querySelector(".btn-outline-danger");
+refresh.addEventListener("click", e => {
+  location.reload();
+});
